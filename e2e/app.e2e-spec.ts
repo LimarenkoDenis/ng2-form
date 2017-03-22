9a8c0f239c6e5e6adb887d9f1678a08dfe782c1c
Limarenko Denis <limarenkodenis@gmail.com>
2017-03-22 17:39:02 +0200

    played with e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,20 @@ describe('ng2-form App', () => {
     page = new Ng2FormPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should have a title', () => {
     page.navigateTo();
+    expect(page.getTitle()).toEqual('Ng2Form');
+  });
+
+  it('should display message saying app works', () => {
     expect(page.getParagraphText()).toEqual('app works!');
   });
+
+  it('should to fill a form with valid values', () => {
+    expect(page.toFillaForm()).toEqual('app works!');
+
+    // page.toFillInputName();
+    // return true;
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,31 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
+// To-Do move all  values to config file
 
 export class Ng2FormPage {
-  navigateTo() {
+  public navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  public getTitle() {
+    return browser.getTitle();
+  }
+
+  public getParagraphText() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  public toFillaForm (): void {
+    element(by.id('name')).sendKeys('My awsome name');
+    element(by.id('surname')).sendKeys('My awsome surname');
+    element(by.id('email')).sendKeys('email@email');
+    element(by.id('email')).sendKeys('1Aqwerty');
+    element(by.id('confirm')).sendKeys('1Aqwerty');
+    element(by.css('app-reactive md-select')).click();
+
+    element(by.id('name')).sendKeys('name');
+    element(by.id('surname')).sendKeys('surname');
+    element(by.id('email')).sendKeys('email@email');
+    element(by.id('email')).sendKeys('1Aqwerty');
+    element(by.id('confirm')).sendKeys('1Aqwerty');
   }
 }
